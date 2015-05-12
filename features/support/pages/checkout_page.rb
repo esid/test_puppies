03180a61454f02a1a6f18261ba5b465b622bdb58
esidorenko <eugene.sidorenko@dataart.com>
2015-05-12 13:19:51 +0200

    Adds using data magic and default.yml

--- features/support/pages/checkout_page.rb
+++ features/support/pages/checkout_page.rb
@@ -1,6 +1,8 @@
 class CheckoutPage
   include PageObject
   include Buttons
+  include DataMagic
+
   text_field(:name, :id => "order_name")
   text_field(:address, :id => "order_address")
   text_field(:email, :id => "order_email")
@@ -8,25 +10,17 @@ class CheckoutPage
   button(:place_order, :value => PLACE_ORDER)
 
   DEFAULT_DATA = {
-      'name' => 'cheezy',
-      'address' => '123 Main Street',
-      'email' => 'cheezy@example.com',
+      'name' => Faker::Name.name,
+      'address' => Faker::Address.street_address,
+      'email' => Faker::Internet.email,
       'pay_type' => 'Purchase order'
  }
 
 
 
   def checkout(data = {})
-    populate_page_with DEFAULT_DATA.merge(data)
+    populate_page_with data_for(:checkout_page, data) #DEFAULT_DATA.merge(data)
     place_order
-=begin
-     data = DEFAULT_DATA.merge(data)
-     self.name = data['name']
-     self.address = data['address']
-     self.email = data['email']
-     self.pay_type = data['pay_type']
-     place_order
-=end
   end
 
 end
